6aaac72f2293f2279aa2ed596154f6d9388f24b0
Hafiz Aqimi <aqimi0103@gmail.com>
2023-12-29 22:50:03 +0800

    q2 new

---
+++ src/main/java/pmujtmk/hafizaqimi/prefinalq2/PreFinalQ2.java
@@ -0,0 +1,16 @@
+// HAFIZ AQIMI BIN JUHAILI
+// 20DDT21F1013
+
+package pmujtmk.hafizaqimi.prefinalq2;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class PreFinalQ2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
